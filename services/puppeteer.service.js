76ebdcfa220e517f103fa878db662b80301994e8
silvtal <stalaveramarcos@gmail.com>
2023-03-22 12:51:18 +0100

    update node; try to fix images

--- services/puppeteer.service.js
+++ services/puppeteer.service.js
@@ -53,35 +53,56 @@ class PuppeteerService {
    */
   async getLatestInstagramPostsFromAccount(acc, n) {
     try {
-//      const page = `https://dumpor.com/v/${acc}`;
-      const page = `https://www.picuki.com/profile/${acc}`;
-      await this.goToPage(page);
+// !!      const page = `https://dumpor.com/v/${acc}`;
+// !!      const page = `https://www.picuki.com/profile/${acc}`;
+// !!      await this.goToPage(page);
+      await page.goto("https://www.picuki.com/profile/${acc}/", {
+        timeout: 80000,
+        }); // !!
       let previousHeight;
 
       console.log('acc', acc);
       console.log('page', page);
 
-      previousHeight = await this.page.evaluate(`document.body.scrollHeight`);
-      await this.page.evaluate(`window.scrollTo(0, document.body.scrollHeight)`);
+// !!      previousHeight = await this.page.evaluate(`document.body.scrollHeight`);
+// !!      await this.page.evaluate(`window.scrollTo(0, document.body.scrollHeight)`);
       // 🔽 Doesn't seem to be needed
       // await this.page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
       // await this.page.waitFor(1000);
-
-      await this.page.waitForSelector('post-image');
-      // In this version of the code, this.page.waitForSelector() is used
-      // to wait for the .post-image selector to appear on the page before
-      // executing this.page.evaluate(). Once the selector is found, the callback
-      // function passed to this.page.evaluate() is executed to extract the src
-      // attribute from each image element and return an array of image URLs.
-      const nodes = await this.page.evaluate(() => {
+      // !! In this version of the code, this.page.waitForSelector() is used
+      // !! to wait for the .post-image selector to appear on the page before
+      // !! executing this.page.evaluate(). Once the selector is found, the callback
+      // !! function passed to this.page.evaluate() is executed to extract the src
+      // !! attribute from each image element and return an array of image URLs.
+      await page.waitForLoadState("networkidle"); // !!
+      console.log("Loaded page!"); // !!
+      console.log("Wait for images..."); // !!
+      await page.waitForSelector(".post-image", { // !!
+        state: "visible", // !!
+      });
+// !!      await this.page.waitForSelector('post-image');
+// !!      const nodes = await this.page.evaluate(() => {
 //        const images = document.querySelectorAll(`.content__img`);
-        const images = document.querySelectorAll(`post-image`);
-        return [].map.call(images, img => img.src);
+// !!        const images = document.querySelectorAll(`post-image`);
+      const data = await page.evaluate(() => {
+        const images: NodeListOf<HTMLImageElement> =
+          document.querySelectorAll(".post-image");
+        const urls = Array.from(images).map((img) => img.src);
+
+        return urls.slice(0, 3);
+      });
+      console.log("Got images!");
+
+      console.log("Closing Browser");
+      await browser.close();
+
+      return(data)
+// !!        return [].map.call(images, img => img.src);
       });
 
-      console.log('nodes', nodes);
+// !!      console.log('nodes', nodes);
 
-      return nodes.slice(0, 3);
+// !!      return nodes.slice(0, 3);
     } catch (error) {
       console.log('Error', error);
       process.exit();
